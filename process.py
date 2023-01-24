5c7988ea6a4b334d04fde494d911bdfa8a355402
caede1008 <79036121+caede1008@users.noreply.github.com>
2023-01-24 12:53:31 +0900

    Delete process.py

--- process.py
+++
@@ -1,6 +0,0 @@
-class Process:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def goPage(self):
-        self.driver.get("https://www.mofa.go.jp/mofaj/link/emblist/europe.html")
